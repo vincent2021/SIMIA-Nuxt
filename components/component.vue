856082dd51c04c88600ad2ca97e99e96ef8cd29c
Vincent MUCCHIELLI <vincent2021@gmail.com>
2023-07-25 14:48:52 +0200

    first commit

---
+++ components/component.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="component-container"></div>
+</template>
+
+<script>
+export default {
+  name: 'AppComponent',
+  props: {},
+}
+</script>
+
+<style scoped>
+.component-container {
+  width: 100%;
+  height: 400px;
+  display: flex;
+  position: relative;
+  align-items: flex-start;
+  flex-direction: column;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,538 @@
+<template>
+  <div class="home-container">
+    <div data-role="Header" class="home-navbar-container">
+      <div class="home-navbar">
+        <div class="home-logo">
+          <img alt="image" src="/logo-200h.png" class="home-image" />
+        </div>
+        <div data-role="MobileMenu" class="home-mobile-menu">
+          <div class="home-top">
+            <img
+              alt="image"
+              src="https://play.teleporthq.io/static/svg/placeholders/no-image.svg"
+              class="home-image1"
+            />
+            <div data-role="CloseMobileMenu" class="home-container1">
+              <svg viewBox="0 0 1024 1024" class="home-icon">
+                <path
+                  d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"
+                ></path>
+              </svg>
+            </div>
+          </div>
+          <div class="home-mid">
+            <div class="home-links-container">
+              <span class="home-link Anchor">features</span>
+              <span class="home-link1 Anchor">services</span>
+              <span class="home-link2 Anchor">About Us</span>
+              <span class="home-link3 Anchor">contact</span>
+            </div>
+            <button class="home-cta-btn Anchor button">sTART BUILDING</button>
+          </div>
+          <div class="home-bot">
+            <div class="home-social-links-container">
+              <svg viewBox="0 0 950.8571428571428 1024" class="home-icon2">
+                <path
+                  d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"
+                ></path></svg
+              ><svg viewBox="0 0 877.7142857142857 1024" class="home-icon4">
+                <path
+                  d="M713.143 73.143c90.857 0 164.571 73.714 164.571 164.571v548.571c0 90.857-73.714 164.571-164.571 164.571h-107.429v-340h113.714l17.143-132.571h-130.857v-84.571c0-38.286 10.286-64 65.714-64l69.714-0.571v-118.286c-12-1.714-53.714-5.143-101.714-5.143-101.143 0-170.857 61.714-170.857 174.857v97.714h-114.286v132.571h114.286v340h-304c-90.857 0-164.571-73.714-164.571-164.571v-548.571c0-90.857 73.714-164.571 164.571-164.571h548.571z"
+                ></path></svg
+              ><svg viewBox="0 0 877.7142857142857 1024" class="home-icon6">
+                <path
+                  d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"
+                ></path>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="home-container2">
+      <img
+        alt="image"
+        src="/cover-1500w.jpg"
+        loading="lazy"
+        class="home-image2"
+      />
+    </div>
+    <div class="home-container3">
+      <h1 class="home-text">
+        NOS JOLIES TÉTINES NATURELLES ONT TIRÉ LEUR RÉVÉRENCE
+      </h1>
+    </div>
+    <div class="home-hero">
+      <div class="home-hero-text-container">
+        <span class="home-text01">
+          <span><span v-html="rawi5lh"></span></span>
+          <span class="home-text03">
+            Elles ont été les complices privilégiées des sommeils apaisés et des
+            nuits parfois plus animées de vos tout petits.
+          </span>
+          <br class="Section-Text" />
+          <br class="Section-Text" />
+          <span class="home-text06">
+            Nous avons eu à cœur de produire en France et d’utiliser de belles
+            matières, parfaitement saines et qui respectent la planète.
+          </span>
+          <br class="Section-Text" />
+          <br class="Section-Text" />
+          <span class="home-text09">
+            Nous espérons de tout cœur que nos tétines en auront inspiré beaucoup
+            d&apos;autres.
+          </span>
+          <br class="Section-Text" />
+          <br class="Section-Text" />
+          <span class="Section-Text">
+            Un immense merci pour vos commandes, les photos si mignonnes de vos
+            tout petits, tous vos mots. Cette aventure a été incroyable grâce à
+            vous.
+          </span>
+          <br class="Section-Text" />
+        </span>
+      </div>
+      <img alt="image" src="/img_right-1500w.webp" class="home-image3" />
+    </div>
+    <a
+      href="mailto:bonjour@simia.co?subject=Contact"
+      class="home-cta-btn1 Anchor button"
+    >
+      NOUS CONTACTER
+    </a>
+    <div class="home-section-separator"></div>
+    <div class="home-footer">
+      <span class="home-text14">© 2023, simia.co avec plein d&apos;amour</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      rawi5lh: ' ',
+    }
+  },
+  head: {
+    title: 'SIMIA',
+    meta: [
+      {
+        property: 'og:title',
+        content: 'SIMIA',
+      },
+    ],
+  },
+}
+</script>
+
+<style scoped>
+.home-container {
+  width: 100%;
+  height: auto;
+  display: flex;
+  min-height: 100vh;
+  align-items: center;
+  flex-direction: column;
+}
+.home-navbar-container {
+  top: 0;
+  width: 100%;
+  display: flex;
+  z-index: 100;
+  position: sticky;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  background-color: var(--dl-color-gray-white);
+}
+.home-navbar {
+  width: 100%;
+  display: flex;
+  position: relative;
+  max-width: var(--dl-size-size-maxcontent);
+  align-items: center;
+  padding-top: var(--dl-space-space-doubleunit);
+  border-color: var(--dl-color-gray-black);
+  padding-left: var(--dl-space-space-tripleunit);
+  border-radius: var(--dl-radius-radius-radius4);
+  padding-right: var(--dl-space-space-tripleunit);
+  flex-direction: row;
+  padding-bottom: var(--dl-space-space-doubleunit);
+  justify-content: space-between;
+}
+.home-logo {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.home-image {
+  width: 125px;
+  object-fit: cover;
+}
+.home-mobile-menu {
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100vh;
+  display: none;
+  padding: var(--dl-space-space-doubleunit);
+  position: absolute;
+  align-items: flex-start;
+  flex-direction: column;
+  background-color: var(--dl-color-gray-white);
+}
+.home-top {
+  flex: 0 0 auto;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin-bottom: var(--dl-space-space-doubleunit);
+  flex-direction: row;
+  justify-content: space-between;
+}
+.home-image1 {
+  width: 50px;
+  object-fit: cover;
+}
+.home-container1 {
+  flex: 0 0 auto;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+}
+.home-icon {
+  width: 24px;
+  height: 24px;
+}
+.home-mid {
+  flex: 0 0 auto;
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+}
+.home-links-container {
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: var(--dl-space-space-doubleunit);
+  flex-direction: column;
+}
+.home-link {
+  margin-bottom: var(--dl-space-space-unit);
+  text-decoration: none;
+}
+.home-link1 {
+  margin-bottom: var(--dl-space-space-unit);
+  text-decoration: none;
+}
+.home-link2 {
+  margin-bottom: var(--dl-space-space-unit);
+  text-decoration: none;
+}
+.home-link3 {
+  text-decoration: none;
+}
+.home-cta-btn {
+  color: var(--dl-color-gray-white);
+  padding-top: var(--dl-space-space-unit);
+  padding-left: var(--dl-space-space-fiveunits);
+  border-radius: 34px;
+  padding-right: var(--dl-space-space-fiveunits);
+  padding-bottom: var(--dl-space-space-unit);
+  background-color: var(--dl-color-primary-100);
+  border-top-width: 0px;
+  border-left-width: 0px;
+  border-right-width: 0px;
+  border-bottom-width: 0px;
+}
+.home-bot {
+  flex: 0 0 auto;
+  width: 200px;
+  display: flex;
+  margin-top: auto;
+  align-items: flex-start;
+  flex-direction: column;
+}
+.home-social-links-container {
+  display: flex;
+  align-self: flex-start;
+  align-items: flex-start;
+  flex-direction: row;
+}
+.home-icon2 {
+  width: 24px;
+  height: 24px;
+  margin-right: var(--dl-space-space-doubleunit);
+}
+.home-icon4 {
+  width: 24px;
+  height: 24px;
+  margin-right: var(--dl-space-space-doubleunit);
+}
+.home-icon6 {
+  width: 24px;
+  height: 24px;
+}
+.home-container2 {
+  flex: 1;
+  width: 100%;
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+}
+.home-image2 {
+  width: 100%;
+  height: 421px;
+  align-self: center;
+  box-shadow: 0px 5px 10px 0px #d4d4d4;
+  object-fit: cover;
+}
+.home-container3 {
+  flex: 0 0 auto;
+  width: 100%;
+  display: flex;
+  margin-top: var(--dl-space-space-tripleunit);
+  align-items: center;
+  margin-left: var(--dl-space-space-doubleunit);
+  margin-right: var(--dl-space-space-doubleunit);
+  margin-bottom: var(--dl-space-space-tripleunit);
+  justify-content: center;
+}
+.home-text {
+  color: rgb(245, 176, 176);
+  width: 893px;
+  height: 158px;
+  font-size: 48px;
+  font-style: normal;
+  margin-top: var(--dl-space-space-halfunit);
+  text-align: center;
+  font-family: BimboFinetip;
+  font-weight: 400;
+  margin-bottom: var(--dl-space-space-fiveunits);
+}
+.home-hero {
+  width: 100%;
+  height: 875px;
+  display: flex;
+  max-width: var(--dl-size-size-maxcontent);
+  align-items: center;
+  padding-top: var(--dl-space-space-fiveunits);
+  padding-left: var(--dl-space-space-tripleunit);
+  margin-bottom: var(--dl-space-space-halfunit);
+  padding-right: var(--dl-space-space-tripleunit);
+  flex-direction: row;
+  padding-bottom: var(--dl-space-space-fiveunits);
+  justify-content: space-between;
+}
+.home-hero-text-container {
+  width: 35%;
+  height: 530px;
+  display: flex;
+  align-items: flex-start;
+  margin-left: 50px;
+  flex-direction: column;
+}
+.home-text01 {
+  color: var(--dl-color-gray-500);
+  align-self: flex-start;
+  margin-bottom: var(--dl-space-space-tenunits);
+}
+.home-image3 {
+  width: 50%;
+  object-fit: contain;
+}
+.home-cta-btn1 {
+  color: var(--dl-color-gray-white);
+  align-self: center;
+  text-align: center;
+  padding-top: var(--dl-space-space-doubleunit);
+  border-width: 0px;
+  border-radius: 34px;
+  margin-bottom: var(--dl-space-space-fiveunits);
+  padding-bottom: var(--dl-space-space-doubleunit);
+  text-decoration: none;
+  background-color: #399;
+}
+.home-section-separator {
+  width: 100%;
+  height: 1px;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  background-color: var(--dl-color-gray-700);
+}
+.home-footer {
+  flex: 0 0 auto;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding-top: var(--dl-space-space-tripleunit);
+  padding-left: var(--dl-space-space-doubleunit);
+  padding-right: var(--dl-space-space-doubleunit);
+  padding-bottom: var(--dl-space-space-unit);
+  justify-content: center;
+  background-color: #79b4ad;
+}
+.home-text14 {
+  color: #ffffff;
+}
+@media(max-width: 1200px) {
+  .home-container2 {
+    box-shadow: 5px 5px 10px 0px #d4d4d4;
+    border-color: var(--dl-color-gray-black);
+    border-radius: var(--dl-radius-radius-radius4);
+  }
+  .home-image2 {
+    border-radius: var(--dl-radius-radius-radius4);
+  }
+  .home-text {
+    color: rgb(245, 176, 176);
+    height: 134px;
+    font-size: 48px;
+    font-style: inherit;
+    font-family: Bimbo Finetip;
+    font-weight: inherit;
+    margin-bottom: 0px;
+  }
+  .home-text01 {
+    color: var(--dl-color-gray-500);
+  }
+  .home-text03 {
+    color: var(--dl-color-gray-500);
+  }
+  .home-text06 {
+    color: var(--dl-color-gray-500);
+  }
+  .home-text09 {
+    color: var(--dl-color-gray-500);
+  }
+  .home-footer {
+    flex: 1;
+    border-color: #000000;
+  }
+}
+@media(max-width: 991px) {
+  .home-image2 {
+    box-shadow: 0px 5px 10px 0px #d4d4d4;
+  }
+  .home-container3 {
+    flex: 1;
+  }
+  .home-text {
+    color: rgb(245, 176, 176);
+    height: 116px;
+    text-align: center;
+    font-family: Bimbo-regular;
+  }
+  .home-hero {
+    height: 1460px;
+    flex-direction: column-reverse;
+  }
+  .home-hero-text-container {
+    width: 80%;
+    margin-left: 0px;
+  }
+  .home-image3 {
+    width: 80%;
+    height: 1057px;
+    margin-top: var(--dl-space-space-tripleunit);
+    margin-bottom: var(--dl-space-space-tripleunit);
+  }
+  .home-cta-btn1 {
+    width: 40%;
+  }
+}
+@media(max-width: 767px) {
+  .home-image2 {
+    flex: 1;
+    height: auto;
+  }
+  .home-container3 {
+    flex: 1;
+    margin-top: var(--dl-space-space-unit);
+  }
+  .home-text {
+    color: rgb(245, 176, 176);
+    width: 767px;
+    height: 56px;
+    font-size: 48px;
+    align-self: center;
+    font-style: normal;
+    text-align: center;
+    font-family: BimboFinetip;
+    font-weight: 400;
+    padding-left: var(--dl-space-space-fourunits);
+    margin-bottom: 0px;
+    padding-right: var(--dl-space-space-fourunits);
+  }
+  .home-hero {
+    flex: 1;
+  }
+  .home-text01 {
+    text-align: center;
+  }
+  .home-image3 {
+    flex: 1;
+  }
+  .home-cta-btn1 {
+    width: auto;
+    align-self: center;
+  }
+  .home-footer {
+    flex: 1;
+    margin-top: var(--dl-space-space-unit);
+  }
+}
+@media(max-width: 479px) {
+  .home-cta-btn {
+    font-size: 16px;
+    padding-left: var(--dl-space-space-doubleunit);
+    padding-right: var(--dl-space-space-doubleunit);
+  }
+  .home-container3 {
+    flex: 1;
+    padding-top: var(--dl-space-space-unit);
+    padding-left: var(--dl-space-space-doubleunit);
+    padding-right: var(--dl-space-space-doubleunit);
+    padding-bottom: var(--dl-space-space-unit);
+  }
+  .home-text {
+    color: rgb(245, 176, 176);
+    height: 21px;
+    font-size: 24px;
+    align-self: center;
+    font-style: normal;
+    text-align: center;
+    font-family: BimboFinetip;
+    font-weight: 400;
+    padding-top: 5px;
+    padding-left: 0px;
+    padding-right: 0px;
+  }
+  .home-hero {
+    flex: 1;
+  }
+  .home-hero-text-container {
+    width: 100%;
+  }
+  .home-text01 {
+    align-self: stretch;
+  }
+  .home-image3 {
+    flex: 1;
+    width: 100%;
+    height: 675px;
+  }
+  .home-footer {
+    flex: 1;
+    padding-top: var(--dl-space-space-unit);
+    padding-left: var(--dl-space-space-doubleunit);
+    padding-right: var(--dl-space-space-doubleunit);
+    padding-bottom: var(--dl-space-space-unit);
+  }
+}
+</style>
